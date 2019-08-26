1ab70cf4540b0e9e85ec3fc53f138c1ba0d45021
Allie McCarthy <mccarthy.allie@gmail.com>
2019-08-26 07:56:14 -0600

    adds vampire and unicorn

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,5 +1,13 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color = "white") {
+    this.name = name;
+    this.color = color;
+  }
+  isWhite() {
+    return false
+  }
+  says(word) {
+    return `**;* ${word} *;**`
   }
 }
 
