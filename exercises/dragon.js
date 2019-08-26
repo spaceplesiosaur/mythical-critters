d638c5b782f0c33a0bed5b227eeba299d9984056
Allie McCarthy <mccarthy.allie@gmail.com>
2019-08-26 15:11:29 -0600

    adds dragon test

---
+++ exercises/dragon.js
@@ -0,0 +1,21 @@
+class Dragon {
+  constructor(name){
+    this.name = name;
+    this.rider = "Eragon";
+    this.color = "blue"
+    this.hungry = true;
+    this.meals = 1;
+  }
+  eat() {
+    if (this.meals < 3) {
+      this.meals++;
+    }
+    else {
+      this.hungry = false;
+    }
+  }
+}
+
+
+
+module.exports = Dragon;
